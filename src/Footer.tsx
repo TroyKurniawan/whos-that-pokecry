52cc61fe358918bd6be84f3efc69fe34bb4b3115
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-04-06 20:14:29 -0700

    Randomly generate and fetch new Pokemon cries

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,10 +1,13 @@
 const Footer = () => {
   return (
     <footer className="w-screen h-20 text-center grid content-center">
-      <p className="text-xs">Deployed with Vercel.</p>
-      <p className="text-xs">Built with React, Typescript, and Tailwind.</p>
-      <br />
-      <p className="text-xs">©️ 2024 Troy Kurniawan</p>
+      <p className="text-xs">
+        Built with <b>React</b>, <b>Typescript</b>, and <b>Tailwind</b>.
+        <br />
+        Deployed with <b>Vercel</b>. Pokémon data API by <b>PokéAPI</b>.
+        <br />
+        ©️ 2024 Troy Kurniawan
+      </p>
     </footer>
   );
 };
