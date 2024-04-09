a2594077aed30c35aa880d742f9f456cf1bf37a1
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-04-09 15:01:11 -0700

    Added magnifying glass to search bar.

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,10 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="w-screen h-20 text-center grid content-center">
+    <footer className="w-screen h-36 text-center grid content-center bottom-0">
       <p className="text-xs">
         Built with <b>React</b>, <b>Typescript</b>, and <b>Tailwind</b>.
         <br />
-        Deployed with <b>Vercel</b>. Pokémon data API by <b>PokéAPI</b>.
+        Deployed with <b>Vercel</b>. Pokémon data API by <b>PokéAPI</b>. Icons
+        by <b>Icons8</b>
         <br />
         ©️ 2024 Troy Kurniawan
       </p>
--- src/Game.tsx
+++ src/Game.tsx
@@ -10,7 +10,7 @@ const Game = () => {
 
   // Generate new url string
   const randomCry = () => {
-    let newUrl =
+    let newUrl: string =
       "https://pokeapi.co/api/v2/pokemon/" +
       (Math.floor(Math.random() * 1025) + 1) +
       "/";
@@ -44,20 +44,35 @@ const Game = () => {
 
   return (
     <div className="h-3/4 grid justify-center content-center">
+      {/* Pokémon's Cry */}
       <div className="w-96 h-auto bg-white grid justify-center content-center rounded-xl m-4">
         <button
           onClick={randomCry}
-          className="bg-red-500 text-white text-2xl p-4 m-4 hover:bg-red-600 rounded-xl"
+          className="bg-red-500 text-white font-bold text-2xl p-4 m-4 hover:bg-red-600 rounded-xl"
         >
           NEW CRY
         </button>
 
-        <audio src={cry} controls className="m-4">
+        <audio src={cry} controls autoPlay className="m-4">
           Your browser does not support the audio tag.
         </audio>
       </div>
 
-      <div>
+      {/* Player Input */}
+      <div className="flex">
+        <div className="absolute m-4 p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="24"
+            height="24"
+            viewBox="0 0 50 50"
+            className="opacity-30"
+          >
+            <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>{" "}
+          </svg>
+        </div>
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -67,7 +82,7 @@ const Game = () => {
           <input
             maxLength={12}
             placeholder="Search Pokémon..."
-            className="w-96 m-4 p-4 border rounded-xl"
+            className="w-96 m-4 pl-14 p-4 rounded-xl"
             type="search"
             id="playerInput"
             required={true}
@@ -75,6 +90,8 @@ const Game = () => {
           {/* <button className="m-4 border">Submit</button> */}
         </form>
       </div>
+
+      <ul></ul>
     </div>
   );
 };
