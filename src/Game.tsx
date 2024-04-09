68885f17a30a70d5fb614c96c28f4f6da0705787
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-04-08 21:01:39 -0700

    Added text field, consolidate game to one component.

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,10 +1,80 @@
-import React from "react";
-import PokemonCry from "./PokemonCry";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Game = () => {
+  const [cry, setCry] = useState();
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/1/");
+  const [currentPokemon, setCurrentPokemon] = useState("");
+
+  // ===============================================
+
+  // Generate new url string
+  const randomCry = () => {
+    let newUrl =
+      "https://pokeapi.co/api/v2/pokemon/" +
+      (Math.floor(Math.random() * 1025) + 1) +
+      "/";
+    console.log(newUrl);
+    setUrl(newUrl);
+  };
+
+  // Get new cry using new url string
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      console.log(res);
+      setCry(res.data.cries.latest);
+      setCurrentPokemon(res.data.name);
+    });
+  }, [url]);
+
+  // Answer is submitted
+  const submitAnswer = () => {
+    const playerAnswer = (document.getElementById(
+      "playerInput"
+    ) as HTMLInputElement)!.value;
+
+    if (playerAnswer.toLowerCase() == currentPokemon.toLowerCase()) {
+      alert("Yippee!");
+    } else {
+      alert("Nah lmao");
+    }
+  };
+
+  // ===============================================
+
   return (
     <div className="h-3/4 grid justify-center content-center">
-      <PokemonCry />
+      <div className="w-96 h-auto bg-white grid justify-center content-center rounded-xl m-4">
+        <button
+          onClick={randomCry}
+          className="bg-red-500 text-white text-2xl p-4 m-4 hover:bg-red-600 rounded-xl"
+        >
+          NEW CRY
+        </button>
+
+        <audio src={cry} controls className="m-4">
+          Your browser does not support the audio tag.
+        </audio>
+      </div>
+
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            submitAnswer();
+          }}
+        >
+          <input
+            maxLength={12}
+            placeholder="Search Pokémon..."
+            className="w-96 m-4 p-4 border rounded-xl"
+            type="search"
+            id="playerInput"
+            required={true}
+          ></input>
+          {/* <button className="m-4 border">Submit</button> */}
+        </form>
+      </div>
     </div>
   );
 };
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="bg-white w-screen h-20 text-center grid content-center">
-      <p className="text-5xl font-bold">WHO'S THAT POKECRY?</p>
+      <p className="text-5xl font-bold">WHO'S THAT POKÉCRY?</p>
     </header>
   );
 };
