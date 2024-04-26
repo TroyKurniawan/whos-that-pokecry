be7a577c7a29914803959200448ac5df83d5d1d1
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-04-26 16:55:54 -0700

    Fixed conditional rendering to display search results

--- src/SearchResultContainer.tsx
+++ src/SearchResultContainer.tsx
@@ -5,22 +5,21 @@ type SearchResultContainerProps = {
 };
 
 const SearchResultContainer = ({ input }: SearchResultContainerProps) => {
-  const container = (document.getElementById("search") as HTMLInputElement)!;
-
+  // If empty, do not display results
   if (input === "") {
-    container.classList.add("hidden");
-  } else {
-    container.classList.remove("hidden");
+    return null;
   }
-
-  return (
-    <div
-      id="search"
-      className="hidden absolute m-4 bottom-16 p-4 w-96 h-96 bg-white justify-center content-center rounded-xl"
-    >
-      <ul>{input}</ul>
-    </div>
-  );
+  // If user types any input, display results
+  else {
+    return (
+      <div
+        id="search"
+        className="absolute m-4 bottom-16 p-4 w-96 h-96 bg-white justify-center content-center rounded-xl"
+      >
+        <ul>{input}</ul>
+      </div>
+    );
+  }
 };
 
 export default SearchResultContainer;
