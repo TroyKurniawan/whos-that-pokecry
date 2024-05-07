83763f8ff691123e7d8df82029aa89835001343c
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-05-07 16:49:51 -0700

    Player answer is now submitted when Pokemon row is clicked

--- src/SearchResultContainer.tsx
+++ src/SearchResultContainer.tsx
@@ -4,20 +4,15 @@ import { PokemonList } from "./assets/pokemon-list.js";
 
 type SearchResultContainerProps = {
   input: string;
+  answer: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const getPokemon = () => {
-  for (let i = 1; i <= 1025; i++) {
-    let url: string = "https://pokeapi.co/api/v2/pokemon/" + i + "/"; // Concat a string of Pokemon from 1 to 1025
-    axios.get(url).then((res) => {
-      return <div> {res.data.name} </div>;
-    });
-  }
-};
-
-const SearchResultContainer = ({ input }: SearchResultContainerProps) => {
+const SearchResultContainer = ({
+  input,
+  answer,
+}: SearchResultContainerProps) => {
   // If empty, do not display results
-  if (input === "") {
+  if (input.length < 1) {
     return null;
   }
   // If user types any input, display results
@@ -27,13 +22,14 @@ const SearchResultContainer = ({ input }: SearchResultContainerProps) => {
         id="search"
         className="overflow-auto absolute m-4 bottom-16 p-4 size-96 size bg-white justify-center rounded-xl"
       >
+        {/* List out Pokemon from pokemon-list.js, then filter out results based on input */}
         {PokemonList.filter((pkmn) =>
           pkmn.name.toLowerCase().includes(input)
         ).map((pkmn) => (
           <div key={pkmn.id} className="grid justify-center">
             <button
               onClick={(e) => {
-                alert("test");
+                answer(pkmn.name);
               }}
               className="text-center items-center font-bold text-xl w-80 h-14 border-b hover:bg-gray-100 cursor-pointer flex"
             >
--- src/assets/generate-pokemon-json.py
+++ src/assets/generate-pokemon-json.py
@@ -6,20 +6,30 @@ read_file = "./src/assets/pokemon-list.txt"
 with open(write_file, 'w', errors='ignore') as w_file:
     with open(read_file, 'r', errors='ignore', encoding='cp1252') as r_file:
         w_file.write("export const PokemonList = [\n")
+
+        # Index "i" for ID number
         i = 1
+
+        # Loop until "line" is empty
         while True:
+
+            # Read line by line
             line = r_file.readline()
 
+            # Break loop
             if not line:
                 w_file.write("]")
                 break
 
+            # Write JSON entry
             line = line.strip()
             w_file.write("  {\n")
             w_file.write("    " + "\"id\": \"" + str(i) + "\",\n")
             w_file.write("    " + "\"name\": \"" + line + "\",\n")
             w_file.write("    " + "\"sprite\": \"" + "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + str(i) + ".png" + "\",\n")
             w_file.write("  },\n")
+
+            # Increment "i"
             i = i + 1
 
-print("Text has been written to", write_file)
+print("Text has been written to", write_file.")
